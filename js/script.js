8d08d20474eceba063fb9944c04e14149ef4b37a
Hyuns610 <hunsik33@naver.com>
2026-06-06 13:44:34 +0900

    메가 메뉴 배경 구현, 1차 메뉴에 마우스 올렸을 때 메가메뉴와 상호작용

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const depth1Lists = document.querySelectorAll(".depth1-list");
+const depth2NavBg = document.querySelector(".depth2-nav-bg");
+
+const activeMenuTexts = ["소개", "투자정보"];
+
+depth1Lists.forEach((list) => {
+  const linkText = list.querySelector(".depth1-link")?.textContent.trim();
+
+  if (activeMenuTexts.includes(linkText)) {
+    list.addEventListener("mouseenter", () => {
+      depth2NavBg.classList.add("active");
+    });
+
+    list.addEventListener("mouseleave", () => {
+      depth2NavBg.classList.remove("active");
+    });
+  }
+});
